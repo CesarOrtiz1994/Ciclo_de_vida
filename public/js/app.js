a36276841e4b8ef4cc9703d21b21085dca76dc46
CesarOrtiz1994 <cesarortize94@gmail.com>
2024-02-02 17:37:54 -0600

    interceptar Fech

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,17 @@
 if (navigator.serviceWorker) {
-    console.log('Hello from service worker!')
     // se registra el service worker
     navigator.serviceWorker.register('/sw.js')
+    // .then(reg => {
+    //     console.log('Service Worker registrado');
+    // })
+    // .catch(err => {
+    //     console.error('Erroral registrar el Service Worker', err);
+    // });
         
 }
+
+    // Solicitud fech
+    fetch('https://fakestoreapi.com/products/1')
+    .then(res => res.json())
+    .then(console.log)
+    .catch(err => console.error('Error al hacer el fetch', err));
--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,29 @@
-//Responder con lo que contiene la solicitud
-self.addEventListener('fetch', event => {
-    console.log(event);
-})
+//ciclo de vidaa del service worker
+//1.- Instalacion
+self.addEventListener('install', event => {
+    console.log('SW instalado');
+    // simular  instalacion de caches
+    const instalacion = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Instalaciones terminadas');
+            self.skipWaiting();
+            resolve();
+        }, 3000);
+        event.waitUntil(instalacion);
+    });
+});
+//2.- Activacion
+self.addEventListener('activate', event => {
+    console.log('SW: activado y controlando la app:');
+});
+//3.- Fetch
+self.addEventListener('fetch', function (event) {
 
-self.addEventListener('fetch', event => {
-    if(event.request.url.includes('boston1')){
-        event.respondWith(fetch('logo512.png'))
+    console.log('SW:', event.request.url);                                                                                  
+
+    if (event.request.url.includes('https://fakestoreapi.com/products/')) {
+    const resp = new Response(`{"ok": false, "mensaje": "interceptar por el SW" }`);
+    event.respondWith(resp);
     }
-})
+
+});
