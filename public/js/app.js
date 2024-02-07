52678a647017ece94b894a565d9685b1f759d793
CesarOrtiz1994 <cesarortize94@gmail.com>
2024-02-07 17:30:58 -0600

    aser push y ver alerta o notificacion

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,25 @@
 if (navigator.serviceWorker) {
-  // se registra el service worker
-  navigator.serviceWorker
-    .register("/sw.js")
-    // .then(reg => {
-    //     console.log('Service Worker registrado');
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((reg) => {
+        Notification.requestPermission().then((permission) => {
+          if (permission === "granted") {
+            reg.showNotification("Hello from SW")
+          }
+        })
+      })
+      .catch((err) => console.log(err))
+    // .then((reg) => {
+    //   setTimeout(() => {
+    //     reg.sync.register("Enviar-fotos")
+    //     console.log("Se enviaron fotos al server")
+    //   }, 5000)
     // })
-    // .catch(err => {
-    //     console.error('Erroral registrar el Service Worker', err);
-    // });
-
-    .then((reg) => {
-        setTimeout(() =>{
-            reg.sync.register('Enviar-fotos')
-            console.log('se enviaron fotos al servidor');
-
-        }, 3000);
-    })
-
-    .catch((err) => {
-        console.error(err);
-    });
-}
+    // .catch((err) => console.error("Error al registrar el service worker", err))
+    // .then((req) => console.log("Service Worker Registered!"))
+    // .catch((err) => console.log("Service Worker Failed to Register!"))
+  }
+  
+  // fetch("https://fakestoreapi.com/products/1")
+  //   .then((res) => res.json())
+  //   .then((data) => console.log(data))
--- public/sw.js
+++ public/sw.js
@@ -1,36 +1,33 @@
-//ciclo de vidaa del service worker
-//1.- Instalacion
-self.addEventListener('install', event => {
-    console.log('SW instalado');
-    // simular  instalacion de caches
+self.addEventListener("install", function (event) {
+    console.log("Service worker installed")
     const instalacion = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log('Instalaciones terminadas');
-            self.skipWaiting();
-            resolve();
-        }, 3000);
-        event.waitUntil(instalacion);
-    });
-});
-//2.- Activacion
-self.addEventListener('activate', event => {
-    console.log('SW: activado y controlando la app:');
-});
-//3.- Fetch
-// self.addEventListener('fetch', function (event) {
-
-//     console.log('SW:', event.request.url);                                                                                  
-
-//     if (event.request.url.includes('https://fakestoreapi.com/products/')) {
-//     const resp = new Response(`{"ok": false, "mensaje": "interceptar por el SW" }`);
-//     event.respondWith(resp);
-//     }
-
-// });
-
-//Sync: Recuperar conexion a internet
-self.addEventListener('sync', event => {
-    console.log('Recuperamos la conexion');
-    console.log(event);
-    console.log(event.tag);
-});
+      setTimeout(() => {
+        console.log("Instalaciones terminadas")
+        self.skipWaiting()
+        resolve()
+      }, 1000)
+    })
+    event.waitUntil(instalacion)
+  })
+  
+  self.addEventListener("activate", function (event) {
+    console.log("Service worker activated")
+  })
+  
+  self.addEventListener("fetch", function (event) {
+    // console.log("Fetch: ", event.request.url)
+    // if (event.request.url.includes("https://fakestoreapi.com/products")) {
+    //   const resp = new Response({"ok": false, "message": "Interceptado por sw"})
+    //   event.respondWith(resp)
+    // }
+  })
+  
+  // self.addEventListener("sync", function (event) {
+  //   console.log("Tenemos conexión")
+  //   console.log(event)
+  //   console.log(event.tag)
+  // })
+  
+  self.addEventListener("push", (event) => {
+    console.log("push")
+  })
