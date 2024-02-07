16ea49230b1d1c90ba8b1779bfced3660369a00b
CesarOrtiz1994 <cesarortize94@gmail.com>
2024-02-07 17:26:46 -0600

    ver lo de asic

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,23 @@
 if (navigator.serviceWorker) {
-    // se registra el service worker
-    navigator.serviceWorker.register('/sw.js')
+  // se registra el service worker
+  navigator.serviceWorker
+    .register("/sw.js")
     // .then(reg => {
     //     console.log('Service Worker registrado');
     // })
     // .catch(err => {
     //     console.error('Erroral registrar el Service Worker', err);
     // });
-        
-}
 
-    // Solicitud fech
-    fetch('https://fakestoreapi.com/products/1')
-    .then(res => res.json())
-    .then(console.log)
-    .catch(err => console.error('Error al hacer el fetch', err));
+    .then((reg) => {
+        setTimeout(() =>{
+            reg.sync.register('Enviar-fotos')
+            console.log('se enviaron fotos al servidor');
+
+        }, 3000);
+    })
+
+    .catch((err) => {
+        console.error(err);
+    });
+}
--- public/sw.js
+++ public/sw.js
@@ -17,13 +17,20 @@ self.addEventListener('activate', event => {
     console.log('SW: activado y controlando la app:');
 });
 //3.- Fetch
-self.addEventListener('fetch', function (event) {
+// self.addEventListener('fetch', function (event) {
 
-    console.log('SW:', event.request.url);                                                                                  
+//     console.log('SW:', event.request.url);                                                                                  
 
-    if (event.request.url.includes('https://fakestoreapi.com/products/')) {
-    const resp = new Response(`{"ok": false, "mensaje": "interceptar por el SW" }`);
-    event.respondWith(resp);
-    }
+//     if (event.request.url.includes('https://fakestoreapi.com/products/')) {
+//     const resp = new Response(`{"ok": false, "mensaje": "interceptar por el SW" }`);
+//     event.respondWith(resp);
+//     }
 
+// });
+
+//Sync: Recuperar conexion a internet
+self.addEventListener('sync', event => {
+    console.log('Recuperamos la conexion');
+    console.log(event);
+    console.log(event.tag);
 });
